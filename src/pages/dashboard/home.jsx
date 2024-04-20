a640f909f0697b1b2cd5f4285fc3dcdfc30c3dc4
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-04-20 02:06:25 -0700

    create table but not now

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -26,6 +26,7 @@ import {
   ordersOverviewData,
 } from "@/data";
 import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
+import { Tables } from '@/pages/dashboard';
 
 export function Home() {
   return (
@@ -48,28 +49,10 @@ export function Home() {
           />
         ))}
       </div>
-      <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
-        {statisticsChartsData.map((props) => (
-          <StatisticsChart
-            key={props.title}
-            {...props}
-            footer={
-              <Typography
-                variant="small"
-                className="flex items-center font-normal text-blue-gray-600"
-              >
-                <ClockIcon strokeWidth={2} className="h-4 w-4 text-blue-gray-400" />
-                &nbsp;{props.footer}
-              </Typography>
-            }
-          />
-        ))}
-      </div>
+
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
-        <Card className="overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm">
 
-        </Card>
-
+            <div className="bg-green-500">HeartIcon</div>
       </div>
     </div>
   );
