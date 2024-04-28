71d8ebc2f01b9ea1c7068087c594e148b6855773
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-04-28 04:55:27 -0700

    nimadur

--- src/admin/admin/tabs.jsx
+++ src/admin/admin/tabs.jsx
@@ -17,6 +17,7 @@ import { DayPicker } from "react-day-picker";
 export function TabsWithWork() {
   const [selectedTab, setSelectedTab] = React.useState("html");
   const [htmlInputValue, setHtmlInputValue] = React.useState("");
+  const [htmlInputValue1, setHtmlInputValue1] = React.useState("");
 
   const handleNextButtonClick = () => {
     if (selectedTab === "html" && !htmlInputValue) {
@@ -30,8 +31,11 @@ export function TabsWithWork() {
   };
 
   const handleHtmlInputChange = (e) => {
-    setHtmlInputValue(e.target.value);
-  };
+   setHtmlInputValue(e.target.value);
+ };
+ const handleHtmlInputChang1e = (e) => {
+  setHtmlInputValue1(e.target.value);
+};
 
   const data = [
     {
@@ -71,8 +75,8 @@ export function TabsWithWork() {
          <textarea
            className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
            placeholder=" "
-           value={htmlInputValue}
-           onChange={handleHtmlInputChange}
+           value={htmlInputValue1}
+           onChange={handleHtmlInputChang1e}
          />
          <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900"
          >
