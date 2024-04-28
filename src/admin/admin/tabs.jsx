06b66f6174e22a16557caab99ec44fe1b29922d2
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-04-28 05:01:25 -0700

    uzgarish

--- src/admin/admin/tabs.jsx
+++ src/admin/admin/tabs.jsx
@@ -6,13 +6,8 @@ import {
   Tab,
   TabPanel,
   Button,
-  Popover,
-  PopoverHandler,
-  Input,
-  PopoverContent,
 } from "@material-tailwind/react";
-import { ChevronLeftIcon, ChevronRightIcon, ForwardIcon } from "@heroicons/react/24/outline";
-import { DayPicker } from "react-day-picker";
+import { ForwardIcon } from "@heroicons/react/24/outline";
 
 export function TabsWithWork() {
   const [selectedTab, setSelectedTab] = React.useState("html");
@@ -31,11 +26,12 @@ export function TabsWithWork() {
   };
 
   const handleHtmlInputChange = (e) => {
-   setHtmlInputValue(e.target.value);
- };
- const handleHtmlInputChang1e = (e) => {
-  setHtmlInputValue1(e.target.value);
-};
+    setHtmlInputValue(e.target.value);
+  };
+
+  const handleHtmlInputChange1 = (e) => {
+    setHtmlInputValue1(e.target.value);
+  };
 
   const data = [
     {
@@ -55,109 +51,90 @@ export function TabsWithWork() {
           </label>
         </div>
       ),
-      button: (
-        <Button
-          className={`flex gap-2 items-center ${
-            selectedTab === "html" && !htmlInputValue ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          onClick={handleNextButtonClick}
-          disabled={selectedTab === "html" && !htmlInputValue}
-        >
-          Next <ForwardIcon className="h-4 w-4 ml-1" />
-        </Button>
-      ),
     },
     {
       label: "Ertangi Ishlar",
       value: "react",
       input: (
-       <div className="relative w-full min-w-[200px]">
-         <textarea
-           className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
-           placeholder=" "
-           value={htmlInputValue1}
-           onChange={handleHtmlInputChang1e}
-         />
-         <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900"
-         >
-           Hisobot Kiriting
-         </label>
-       </div>
-     ),
-      button: (
-       <Button
-       className={`flex gap-2 items-center ${
-         selectedTab === "html" && !htmlInputValue ? "opacity-50 cursor-not-allowed" : ""
-       }`}
-       onClick={handleNextButtonClick}
-       disabled={selectedTab === "html" && !htmlInputValue}
-     >
-       Next <ForwardIcon className="h-4 w-4 ml-1" />
-     </Button>
+        <div className="relative w-full min-w-[200px]">
+          <textarea
+            className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+            placeholder=" "
+            value={htmlInputValue1}
+            onChange={handleHtmlInputChange1}
+          />
+          <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900"
+          >
+            Hisobot Kiriting
+          </label>
+        </div>
       ),
     },
     {
-     label: "Ish Qurollari",
-     value: "css",
-     input:(
-     <div className="relative w-full min-w-[200px]">
-     <table className="min-w-full divide-y divide-gray-200">
-       <thead className="bg-gray-50">
-         <tr>
-           <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-             Oylik toʻlovlar
-           </th>
-           <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-             Taomlar
-           </th>
-           <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-             Foydalanuvchi
-           </th>
-           <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-             Chek
-           </th>
-         </tr>
-       </thead>
-       <tbody className="bg-white divide-y divide-gray-200">
-         <tr>
-           <td className="px-6 py-4 whitespace-nowrap">
-             <div className="text-sm text-gray-900">250</div>
-           </td>
-           <td className="px-6 py-4 whitespace-nowrap">
-             <div className="text-sm text-gray-900">Ko'p sevimli taomlar</div>
-           </td>
-           <td className="px-6 py-4 whitespace-nowrap">
-             <div className="text-sm text-gray-900">Ali</div>
-           </td>
-           <td className="px-6 py-4 whitespace-nowrap">
-             <input type="checkbox" className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-           </td>
-         </tr>
-         {/* Additional rows go here */}
-       </tbody>
-     </table>
-   </div>
-     ),
-     button: (
-       <Button
-         className={`flex gap-2 items-center ${
-           selectedTab === "html" && !htmlInputValue ? "opacity-50 cursor-not-allowed" : ""
-         }`}
-         onClick={handleNextButtonClick}
-         disabled={selectedTab === "html" && !htmlInputValue}
-       >
-         Yuborish
-       </Button>
-     ),
-   },
+      label: "Ish Qurollari",
+      value: "css",
+      input: (
+        <div className="relative w-full min-w-[200px]">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Oylik toʻlovlar
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Taomlar
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Foydalanuvchi
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Chek
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <tr>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-900">250</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-900">Ko'p sevimli taomlar</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-900">Ali</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <input
+                    type="checkbox"
+                    className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                </td>
+              </tr>
+              {/* Additional rows go here */}
+            </tbody>
+          </table>
+        </div>
+      ),
+    },
   ];
 
   return (
     <div className="mt-5 mb-10 flex flex-col gap-10">
-    <div>
-      <h3>PD-report</h3>
-      {/* for calendar */}
-
+      <div>
+        <h3>PD-report</h3>
+        {/* for calendar */}
       </div>
       <Tabs
         id="custom-animation"
@@ -178,10 +155,20 @@ export function TabsWithWork() {
             unmount: { y: 250 },
           }}
         >
-          {data.map(({ value, input, button }) => (
+          {data.map(({ value, input }) => (
             <TabPanel key={value} value={value} className="flex flex-col gap-2">
               {input}
-              {button}
+              <Button
+                className={`flex gap-2 items-center ${
+                  selectedTab === "html" && !htmlInputValue
+                    ? "opacity-50 cursor-not-allowed"
+                    : ""
+                }`}
+                onClick={handleNextButtonClick}
+                disabled={selectedTab === "html" && !htmlInputValue}
+              >
+                Next <ForwardIcon className="h-4 w-4 ml-1" />
+              </Button>
             </TabPanel>
           ))}
         </TabsBody>
