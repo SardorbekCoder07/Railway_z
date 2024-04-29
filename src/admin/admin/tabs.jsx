2916c1c43c30ee8026b49dc78a608bb982297d70
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-04-29 13:58:41 +0300

    wef

--- src/admin/admin/tabs.jsx
+++ src/admin/admin/tabs.jsx
@@ -128,12 +128,7 @@ export function TabsWithWork() {
                 >
                   Qiymati
                 </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Chek
-                </th>
+                
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
