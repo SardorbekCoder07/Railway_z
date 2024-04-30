e70e95b8bc53df7c65cc42ea57bc6e87a27f1e19
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-04-30 06:00:20 -0700

    landing page update

--- src/landing_page/home.jsx
+++ src/landing_page/home.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import Navbar from './navbar';
 
 const HomePage = () => {
-  const onSign = () => document.getElementById("sign").click()
-  return (
-    <div>
-    <Link id='sign' to={"/auth/log-in"}></Link>
-     <h1>Landing page</h1>
-     <button className='bg-green-500 p-3 ml-4 mt-3 rounded-xl px-5' onClick={() => onSign()}>Sign Up</button>
-    </div>
-  )
-}
 
+return(
+  <div>
+    {/* Navbar */}
+      <Navbar/>
+  </div>
+)
+}
 export default HomePage
