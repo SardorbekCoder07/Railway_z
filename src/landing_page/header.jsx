77cdf2050f546fcb3f2b980b934d84ce6332d33f
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-05-02 05:00:29 -0700

    kirish add btn with link

--- src/landing_page/header.jsx
+++ src/landing_page/header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import {
     Navbar,
     MobileNav,
@@ -109,6 +110,9 @@ const Header = () => {
                     </Button>
                 </a>
             </Typography>
+            <Link to={"/auth/log-in"}>
+                <Button>Kirish</Button>
+            </Link>
         </ul>
     );
     return (
