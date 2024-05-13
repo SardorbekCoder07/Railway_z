e22da67ed2a17b7cec55bcf069dd6ef3f063f960
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-05-13 04:05:01 -0700

    wq

--- src/superAdmin/dashboard/pdUsers.jsx
+++ src/superAdmin/dashboard/pdUsers.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
     Card,
     CardHeader,
@@ -13,13 +13,13 @@ import {
     Select,
     Option,
 } from "@material-tailwind/react";
-import {MdDelete} from "react-icons/md";
-import {FaEdit} from "react-icons/fa";
-import {authorsTableData} from "@/superAdmin/data";
-import {CircularPagination} from "@/superAdmin/widgets/layout/circlePagination";
-import {UserPlusIcon} from "@heroicons/react/24/solid";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+import { authorsTableData } from "@/superAdmin/data";
+import { CircularPagination } from "@/superAdmin/widgets/layout/circlePagination";
+import { UserPlusIcon } from "@heroicons/react/24/solid";
 import axios from "axios";
-import {api, byId, config, setConfig} from "@/api/api";
+import { api, byId, config, setConfig } from "@/api/api";
 import toast from "react-hot-toast";
 
 export function PdUsers() {
@@ -56,7 +56,7 @@ export function PdUsers() {
     const getUser = () => {
         axios.get(`${api}user/leader/no/pd`, config)
             .then((res) => {
-                 setUsers(res.data.body);
+                setUsers(res.data.body);
             })
             .catch((err) => {
             })
@@ -129,7 +129,7 @@ export function PdUsers() {
 
             })
             .catch((err) => {
-               toast.error("PD o'chirishda xatolik yuz berdi")
+                toast.error("PD o'chirishda xatolik yuz berdi")
                 closeDeleteModal()
             })
 
@@ -160,7 +160,7 @@ export function PdUsers() {
             setRegex(true)
         }
     }
-     return (
+    return (
         <div className="mt-12 mb-8 flex flex-col gap-12 ">
             <Card>
                 <CardHeader variant="gradient" color="gray" className="mb-8 flex items-center justify-between p-6">
@@ -170,35 +170,35 @@ export function PdUsers() {
                     <Button
                         onClick={openAddModal}
                         className="bg-[#fff] text-black px-3 py-2 rounded-md">
-                        <UserPlusIcon className="h-6 w-6 text-black"/>
+                        <UserPlusIcon className="h-6 w-6 text-black" />
                     </Button>
                 </CardHeader>
                 <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
                     <table className="w-full min-w-[640px] table-auto">
                         <thead>
-                        <tr>
-                            {["#", "Bo'linma", "Ishchilar soni", "Yo'l Ustalari", "Amallar"].map((el) => (
-                                <th
-                                    key={el}
-                                    className="border-b border-blue-gray-50 py-3 px-5 text-left"
-                                >
-                                    <Typography
-                                        variant="small"
-                                        className="text-[11px] font-bold uppercase text-blue-gray-400"
+                            <tr>
+                                {["#", "Bo'linma", "Ishchilar soni", "Yo'l Ustalari", "Amallar"].map((el) => (
+                                    <th
+                                        key={el}
+                                        className="border-b border-blue-gray-50 py-3 px-5 text-left"
                                     >
-                                        {el}
-                                    </Typography>
-                                </th>
-                            ))}
-                        </tr>
+                                        <Typography
+                                            variant="small"
+                                            className="text-[11px] font-bold uppercase text-blue-gray-400"
+                                        >
+                                            {el}
+                                        </Typography>
+                                    </th>
+                                ))}
+                            </tr>
                         </thead>
                         <tbody>
 
-                        {PD ? PD.map((item, i) => {
+                            {PD ? PD.map((item, i) => {
                                 const className = `py-3 px-5  ${i === PD && PD.length - 1
                                     ? ""
                                     : "border-b border-blue-gray-50"
-                                }`
+                                    }`
                                 return (
 
                                     <tr key={i}>
@@ -242,34 +242,34 @@ export function PdUsers() {
                                                 openEditModal()
                                                 setUserData(item)
                                             }}
-                                                        className=" cursor-pointer text-[1.2rem] font-semibold hover:text-yellow-300 duration-150 ease-in-out   text-blue-gray-600">
-                                                <FaEdit/>
+                                                className=" cursor-pointer text-[1.2rem] font-semibold hover:text-yellow-300 duration-150 ease-in-out   text-blue-gray-600">
+                                                <FaEdit />
                                             </Typography>
                                             <Typography onClick={() => {
                                                 openDeleteModal()
                                                 setUserData(item)
                                             }}
-                                                        className=" cursor-pointer text-[1.2rem] font-semibold hover:text-red-300 duration-150 ease-in-out text-blue-gray-600">
-                                                <MdDelete/>
+                                                className=" cursor-pointer text-[1.2rem] font-semibold hover:text-red-300 duration-150 ease-in-out text-blue-gray-600">
+                                                <MdDelete />
                                             </Typography>
                                         </td>
                                     </tr>
                                 )
                             }
-                        ) : (
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td className="text-center">
-                                    <Typography
-                                        className=" cursor-pointer text-md font-semibold hover:text-red-300 duration-150 ease-in-out text-blue-gray-600">
-                                        Malumot yo'q
-                                    </Typography></td>
-                                <td></td>
-                                <td></td>
+                            ) : (
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td className="text-center">
+                                        <Typography
+                                            className=" cursor-pointer text-md font-semibold hover:text-red-300 duration-150 ease-in-out text-blue-gray-600">
+                                            Malumot yo'q
+                                        </Typography></td>
+                                    <td></td>
+                                    <td></td>
 
-                            </tr>
-                        )}
+                                </tr>
+                            )}
 
                         </tbody>
                     </table>
@@ -284,12 +284,12 @@ export function PdUsers() {
                         <div className="flex justify-center flex-col items-center gap-7">
                             <div className="w-full max-w-[24rem]">
                                 <Input onChange={editRegex} required defaultValue={userData ? userData.name : "Ma'lumot yo'q"}
-                                       id="editPD" label="Bo'linma nomi"/>
+                                    id="editPD" label="Bo'linma nomi" />
                             </div>
                             <div className="w-full max-w-[24rem]">
                                 <Input onChange={editRegex} required
-                                       defaultValue={userData ? userData.employeeCount : "Ma'lumot yo'q"} id="editemployeeCount"
-                                       label="Ishchi soni"/>
+                                    defaultValue={userData ? userData.employeeCount : "Ma'lumot yo'q"} id="editemployeeCount"
+                                    label="Ishchi soni" />
                             </div>
                             <div className="w-full max-w-[24rem]">
                                 <Select onChange={(e) => {
@@ -319,10 +319,10 @@ export function PdUsers() {
                         </Button>
                         <span className={`${regex ? "cursor-not-allowed" : ""}`}>
 
-              <Button disabled={regex} onClick={editPd} variant="gradient" color="gray">
-                <span>Tahrirlash</span>
-              </Button>
-            </span>
+                            <Button disabled={regex} onClick={editPd} variant="gradient" color="gray">
+                                <span>Tahrirlash</span>
+                            </Button>
+                        </span>
                     </DialogFooter>
                 </Dialog>
             </div>
@@ -335,10 +335,10 @@ export function PdUsers() {
                     <DialogBody>
                         <div className="flex justify-center flex-col items-center gap-7">
                             <div className="w-full max-w-[24rem]">
-                                <Input onChange={addRegex} required id="addPD" label="PD nomi"/>
+                                <Input onChange={addRegex} required id="addPD" label="PD nomi" />
                             </div>
                             <div className="w-full max-w-[24rem]">
-                                <Input onChange={addRegex} required id="addemployeeCount" label="Ishchi soni"/>
+                                <Input onChange={addRegex} required id="addemployeeCount" label="Ishchi soni" />
                             </div>
                             <div className="w-full max-w-[24rem]">
                                 <Select onChange={(e) => {
@@ -368,10 +368,10 @@ export function PdUsers() {
                         </Button>
                         <span className={`${regex ? "cursor-not-allowed" : ""}`}>
 
-              <Button disabled={regex} onClick={addPD} variant="gradient" color="gray">
-                <span>Qo'shish</span>
-              </Button>
-            </span>
+                            <Button disabled={regex} onClick={addPD} variant="gradient" color="gray">
+                                <span>Qo'shish</span>
+                            </Button>
+                        </span>
                     </DialogFooter>
                 </Dialog>
             </div>
--- src/superAdmin/widgets/layout/dashboard-navbar.jsx
+++ src/superAdmin/widgets/layout/dashboard-navbar.jsx
@@ -11,6 +11,10 @@ import {
   MenuList,
   MenuItem,
   Avatar,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
 } from "@material-tailwind/react";
 import {
   UserCircleIcon,
@@ -25,27 +29,32 @@ import {
   setOpenConfigurator,
   setOpenSidenav,
 } from "@/context";
+import { FaEdit } from "react-icons/fa";
+import { useState } from "react";
 
 export function DashboardNavbar() {
   const [controller, dispatch] = useMaterialTailwindController();
   const { fixedNavbar, openSidenav } = controller;
   const { pathname } = useLocation();
   const [layout, page] = pathname.split("/").filter((el) => el !== "");
-
+  const [editModal, setEditModal] = useState(false)
+  const openEditModal = () => setEditModal(true)
+  const closeEditModal = () => {
+      setEditModal(false)
+  }
   return (
     <Navbar
       color={"white"}
       className={`rounded-xl transition-all ${"sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5"
-      }`}
+        }`}
       fullWidth
       blurred={fixedNavbar}
     >
       <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
         <div className="capitalize">
           <Breadcrumbs
-            className={`bg-transparent p-0 transition-all ${
-              fixedNavbar ? "mt-1" : ""
-            }`}
+            className={`bg-transparent p-0 transition-all ${fixedNavbar ? "mt-1" : ""
+              }`}
           >
             <Link to={`/${layout}/boshqaruv-paneli`}>
               <Typography
@@ -70,7 +79,7 @@ export function DashboardNavbar() {
         </div>
         <div className="flex items-center justify-between">
 
-        <IconButton
+          <IconButton
             variant="text"
             color="blue-gray"
             className="grid xl:hidden"
@@ -79,85 +88,129 @@ export function DashboardNavbar() {
             <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
           </IconButton>
 
-          {/* <Menu>
+          <Menu>
             <MenuHandler>
-              <IconButton variant="text" color="blue-gray">
-                <BellIcon className="h-5 w-5 text-blue-gray-500" />
-              </IconButton>
-            </MenuHandler>
-            <MenuList className="w-max border-0">
-              <MenuItem className="flex items-center gap-3">
+              <div className="flex items-center justify-center gap-2">
+
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="mb-1 font-normal"
+                >
+                  <strong>Ism</strong>
+                </Typography>
                 <Avatar
-                  src="https://demos.creative-tim.com/material-dashboard/assets/img/team-2.jpg"
-                  alt="item-1"
-                  size="sm"
-                  variant="circular"
-                />
-                <div>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-1 font-normal"
-                  >
-                    <strong>New message</strong> from Laur
-                  </Typography>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="flex items-center gap-1 text-xs font-normal opacity-60"
-                  >
-                    <ClockIcon className="h-3.5 w-3.5" /> 13 minutes ago
-                  </Typography>
-                </div>
-              </MenuItem>
-              <MenuItem className="flex items-center gap-4">
-                <Avatar
+                  className="cursor-pointer"
                   src="https://demos.creative-tim.com/material-dashboard/assets/img/small-logos/logo-spotify.svg"
                   alt="item-1"
-                  size="sm"
+                  size="md"
                   variant="circular"
                 />
+              </div>
+            </MenuHandler>
+            <MenuList className="w-max border-0">
+              <MenuItem onClick={openEditModal} className="flex items-center justify-center">
+                <FaEdit className={'text-[1.2rem]'} />
+              </MenuItem>
+              <MenuItem className="flex items-center gap-3">
+
                 <div>
                   <Typography
                     variant="small"
                     color="blue-gray"
                     className="mb-1 font-normal"
                   >
-                    <strong>New album</strong> by Travis Scott
+                    <strong>Ism, familiya</strong>
                   </Typography>
                   <Typography
                     variant="small"
                     color="blue-gray"
                     className="flex items-center gap-1 text-xs font-normal opacity-60"
                   >
-                    <ClockIcon className="h-3.5 w-3.5" /> 1 day ago
+                    Ali valiyev
                   </Typography>
                 </div>
               </MenuItem>
               <MenuItem className="flex items-center gap-4">
-                <div className="grid h-9 w-9 place-items-center rounded-full bg-gradient-to-tr from-blue-gray-800 to-blue-gray-900">
-                  <CreditCardIcon className="h-4 w-4 text-white" />
-                </div>
                 <div>
                   <Typography
                     variant="small"
                     color="blue-gray"
                     className="mb-1 font-normal"
                   >
-                    Payment successfully completed
+                    <strong>Telefon raqam </strong>
                   </Typography>
                   <Typography
                     variant="small"
                     color="blue-gray"
                     className="flex items-center gap-1 text-xs font-normal opacity-60"
                   >
-                    <ClockIcon className="h-3.5 w-3.5" /> 2 days ago
+                    +998972220790
+                  </Typography>
+                </div>
+              </MenuItem>
+              <MenuItem className="flex items-center gap-4">
+                <div>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="mb-1 font-normal"
+                  >
+                    <strong>Parol</strong>
+                  </Typography>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="flex items-center gap-1 text-xs font-normal opacity-60"
+                  >
+                    *******
                   </Typography>
                 </div>
               </MenuItem>
             </MenuList>
-          </Menu> */}
-          
+          </Menu>
+
+          <div>
+
+        <Dialog open={editModal} handler={closeEditModal}>
+          <DialogHeader>Tahrirlash</DialogHeader>
+          <DialogBody>
+            <div className="flex justify-center flex-col items-center gap-7">
+              <div className="w-full max-w-[24rem]">
+                <Input
+                  id="editName" label="Ism" />
+              </div>
+              <div className="w-full max-w-[24rem]">
+                <Input
+                  id="editPhone"
+                  label="Telefon raqam" />
+              </div>
+              <div className="w-full max-w-[24rem]">
+                <Input
+                  id="editPassword"
+                  label="Parol" />
+              </div>
+            </div>
+          </DialogBody>
+          <DialogFooter>
+            <Button
+              variant="text"
+              color="red"
+              onClick={closeEditModal}
+              className="mr-1"
+            >
+              <span>Orqaga</span>
+            </Button>
+            <span >
+
+              <Button variant="gradient" color="gray">
+                <span>Tahrirlash</span>
+              </Button>
+            </span>
+          </DialogFooter>
+        </Dialog>
+        </div>
+
         </div>
       </div>
     </Navbar>
